c2727c58504b37515bc81a3160bbe0d3acf6a689
RyanSamman <RyanSamman@outlook.com>
2020-07-30 10:16:55 +0300

    Cleaned up server code & refactored it

--- server.js
+++ server.js
@@ -1,31 +1,35 @@
 // io needs to use HTTP, express will still be the middleware for routes
 const express = require('express')
 const app = express(); //creates the express app
-const httpserver = require('http').createServer(app); //app is an http server
-const io = require('socket.io')(httpserver);
+const http = require('http')
+const socketIO = require('socket.io');
+const path = require('path')
 
-const port = process.env.PORT || 3000;
-
-// http server listening on port
-httpserver.listen(port, function(){
-    console.log('listening on *:' + port);
-  });
-
-  app.use("/public", express.static('./public/')); //The folder that houses all our files
+app.use("/public", express.static('./public/')); //The folder that houses all our files
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-  io.on('connection', function(socket){
-    console.log('client is connected ' + socket.id)
+// http server listening on port
+server = http.createServer(app)
 
-    socket.on('userMessage', (data) => {
-        io.sockets.emit('userMessage', data)
-    });
+const port = process.env.PORT || 3000;
+server.listen(port, function(){
+  console.log('listening on *:' + port);
+});
 
-    socket.on('userTyping', (data) => {
-        socket.broadcast.emit('userTyping', data)
-    })
+io = socketIO(server)
 
+io.on('connection', function(socket){
+  console.log('client is connected ' + socket.id)
+
+  socket.on('userMessage', (data) => {
+      io.sockets.emit('userMessage', data)
   });
+
+  socket.on('userTyping', (data) => {
+      socket.broadcast.emit('userTyping', data)
+  })
+
+});
